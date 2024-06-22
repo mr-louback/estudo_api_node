b3c951613ade6ab0c74d088239bd71b5f044955c
mr-louback <loubacked4work@outlook.com>
2024-06-22 12:35:25 -0300

    rotaCEP

--- api/index.js
+++ api/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000 ;
 const cors = require('cors')
+const cepRoute = require('./routes/cep')
 
 
 app.use(cors())
-app.get('/hello', (req, res) => {
-  res.json({message: 'Hello World from Vercel!'});
-});
+
+app.use('/cep', cepRoute)
 
 app.listen(port, () => {
-  console.log(`API listening at http://localhost:${port}`);
+  console.log(`API listening... ${port} `);
 });
 
 module.exports = app;
